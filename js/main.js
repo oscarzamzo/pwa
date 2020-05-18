9e2966f7f618474e31c156f5820a024318be44bb
Oscar Zambrano <oscarzam866@gmail.com>
2020-05-18 14:15:00 -0600

    Commit Inicial

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//Service Worker 
+
+//if("serviceWorker" in navigator){
+if("serviceWorker" in navigator){
+	console.log("Puedes usar Service Worker");
+
+	//navigator.serviceWorker.register("./js/ServicioWorker.js")
+	navigator.serviceWorker.register("./js/sw.js")
+	.then(res=> console.log("ServiceWorker cargado correctamente",res))
+	.catch(err=> console.log("No se pudo cargar el ServiceWorker",err))
+}else{
+	console.log("No puedes usar Service Worker")
+
+}
+
+
+$(document).ready(function(){
+	/*alert("Hola Mundo...!!");*/
+	$("#menu a").click(function(e){
+		e.preventDefault();
+
+		//console.log($("#servicios").offset().top);
+		//Suabizar Scroll
+		$("html, body").animate({
+			scrollTop:$($(this).attr("href")).offset().top
+		});
+
+		return false;
+	});
+});
