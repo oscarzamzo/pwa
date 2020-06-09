e8da39bd731d87c6e206e290bfce79de619bff73
oscarzamzo <59297813+oscarzamzo@users.noreply.github.com>
2020-06-09 11:07:27 -0600

    update swjs
    update sw.js 09062020

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ const CACHE_NAME="V1_cache_Rentas_y_Rentas_sw2";
 // Ficheros a cachear en las aplicacion parece que este no es
 var urlsToCache=[
 	'./',
-	'./css/estilos.css',
-	'./img/favicon.png',
+	'/css/estilos.css',
+	'/img/favicon.png'/*,
 	'./img/1.png',
 	'./img/2.png',
 	'./img/3.png',
@@ -25,7 +25,7 @@ var urlsToCache=[
 	'./img/favicon-96.png',
 	'./img/favicon-64.png',
 	'./img/favicon-32.png',
-	'./img/favicon-16.png'
+	'./img/favicon-16.png'*/
 ];
 
 //Evento install
